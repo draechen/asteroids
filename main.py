e8d44bf1e123dd1b371048343306197d654444ff
draechen <dmrennie@hotmail.com>
2025-02-17 14:26:13 -0500

    Drop files from .gitignore

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
     color = (0,0,0)
+    clock = pygame.time.Clock()
+    dt = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color)
         pygame.display.flip()
-
+        delta = clock.tick(60)
+        dt = delta / 1000
 if __name__ == "__main__":
     main()
